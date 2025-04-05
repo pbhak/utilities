803f634fe6fa76b13aea60d8b66947af9781f290
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-04-05 04:19:23 +0000

    fix issue with path

--- src/bot.js
+++ src/bot.js
@@ -18,7 +18,7 @@ const bot = new Bolt.App({
 
 const CHANNEL = 'C01KPAX6AG2'; // C01KPAX6AG2 for #bot-testing-ground, C08H2P5RHA7 for #parneel-yaps
 
-const messages = yaml.load(readFileSync('src/messages.yml'));
+const messages = yaml.load(readFileSync('/home/pbhak/utilities/src/messages.yml'));
 
 bot.event('message', async event => {
   try {
