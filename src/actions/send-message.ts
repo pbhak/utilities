253ca9ab8b3f08bb01c5a7f495bb1c1b91ff9ff8
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-05-04 13:01:21 -0700

    add message/reply actions

---
+++ src/actions/send-message.ts
@@ -0,0 +1,129 @@
+import type {
+  AllMiddlewareArgs,
+  BlockAction,
+  SlackActionMiddlewareArgs,
+  SlackViewMiddlewareArgs,
+  ViewSubmitAction,
+} from '@slack/bolt';
+import { transcript } from '..';
+
+interface MessageMetadata {
+  ts: string;
+  cid: string;
+  uid: string;
+  doNotWelcome: boolean;
+}
+
+// TODO add jsdoc comments instead of long normal ones
+
+export async function welcomeInjection({ context, next }: AllMiddlewareArgs): Promise<void> {
+  // When the user replies to a message through their DMs with the bot, and not through
+  // the welcome message, this middleware is passed through to make doNotWelcome true,
+  // so that the bot doesn't try to edit out a send message button from a nonexistent welcome message.
+  context.doNotWelcome = true;
+  await next();
+}
+
+export async function openMessageView({
+  ack,
+  body,
+  client,
+  context,
+}: SlackActionMiddlewareArgs<BlockAction> & AllMiddlewareArgs): Promise<void> {
+  await ack();
+
+  await client.views.open({
+    trigger_id: body.trigger_id,
+    view: {
+      callback_id: 'messageViewSubmitted',
+      type: 'modal',
+      private_metadata: JSON.stringify({
+        ts: body.message?.ts,
+        cid: body.container.channel_id,
+        uid: body.user.id,
+        doNotWelcome: context.doNotWelcome,
+      }),
+      title: {
+        type: 'plain_text',
+        text: 'top text',
+      },
+      submit: {
+        type: 'plain_text',
+        text: 'send',
+      },
+      close: {
+        type: 'plain_text',
+        text: 'cancel',
+      },
+      blocks: [
+        {
+          type: 'input',
+          block_id: 'message',
+          label: {
+            type: 'plain_text',
+            text: 'message to send:',
+          },
+          element: {
+            type: 'plain_text_input',
+            action_id: 'input',
+          },
+        },
+      ],
+    },
+  });
+}
+
+export async function handleMessageSubmission({
+  ack,
+  body,
+  client,
+  payload,
+  respond,
+}: SlackViewMiddlewareArgs<ViewSubmitAction> & AllMiddlewareArgs): Promise<void> {
+  await ack({ response_action: 'clear' });
+
+  const metadata: MessageMetadata = JSON.parse(payload.private_metadata);
+
+  if (!metadata.doNotWelcome) {
+    // Since doNotWelcome was false, we know the user got here from a welcome message
+    // Update the welcome message to remove the "send message" button
+    await client.chat.update({
+      channel: metadata.cid,
+      ts: metadata.ts,
+      text: transcript.welcome.public.replace('{user}', `<@${body.user.id}>`),
+    });
+  }
+
+  await client.chat.postEphemeral({
+    channel: metadata.cid,
+    user: metadata.uid,
+    text: 'message sent!',
+  });
+
+  await client.chat.postMessage({
+    channel: process.env.USER_ID,
+    text: `<@${metadata.uid}> said: ${payload.state.values.message?.input?.value}`,
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `<@${metadata.uid}> said: ${payload.state.values.message?.input?.value}`,
+        },
+      },
+      {
+        type: 'actions',
+        elements: [
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'reply',
+            },
+            action_id: 'replyClicked', // FIXME
+          },
+        ],
+      },
+    ],
+  });
+}
---
+++ src/actions/send-reply.ts
@@ -0,0 +1,106 @@
+import type {
+  AllMiddlewareArgs,
+  BlockAction,
+  SlackActionMiddlewareArgs,
+  SlackViewMiddlewareArgs,
+  ViewSubmitAction,
+} from '@slack/bolt';
+
+interface ReplyMetadata {
+  ts: string;
+  cid: string;
+  uid: string;
+  message: string;
+}
+
+export async function openReplyView({
+  ack,
+  body,
+  client,
+}: SlackActionMiddlewareArgs<BlockAction> & AllMiddlewareArgs): Promise<void> {
+  await ack();
+
+  if (!body.message || !body.message.text) return;
+
+  await client.views.open({
+    trigger_id: body.trigger_id,
+    view: {
+      callback_id: 'replyViewSubmitted', // FIXME
+      type: 'modal',
+      private_metadata: JSON.stringify({
+        ts: body.message?.ts,
+        cid: body.container.channel_id,
+        uid: body.message.text.match(/<@(\w+)>/)![1],
+        message: body.message.text.match(/(?<=said: ).*/)![0],
+      }),
+      title: {
+        type: 'plain_text',
+        text: 'top text',
+      },
+      submit: {
+        type: 'plain_text',
+        text: 'send',
+      },
+      close: {
+        type: 'plain_text',
+        text: 'cancel',
+      },
+      blocks: [
+        {
+          type: 'input',
+          block_id: 'reply',
+          label: {
+            type: 'plain_text',
+            text: 'reply:',
+          },
+          element: {
+            type: 'plain_text_input',
+            action_id: 'input',
+          },
+        },
+      ],
+    },
+  });
+}
+
+export async function handleReplySubmission({
+  ack,
+  client,
+  payload,
+}: SlackViewMiddlewareArgs & AllMiddlewareArgs): Promise<void> {
+  await ack();
+
+  const metadata: ReplyMetadata = JSON.parse(payload.private_metadata);
+
+  await client.chat.update({
+    ts: metadata.ts,
+    channel: metadata.cid,
+    text: `> ${payload.state.values.reply?.input?.value} \nreply sent!`,
+  });
+
+  await client.chat.postMessage({
+    channel: metadata.uid,
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `> ${metadata.message} \n<@U07V1ND4H0Q> replied: ${payload.state.values.reply?.input?.value}`,
+        },
+      },
+      {
+        type: 'actions',
+        elements: [
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'reply',
+            },
+            action_id: 'replyAgain', // FIXME
+          },
+        ],
+      },
+    ],
+  });
+}
