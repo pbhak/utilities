c5d51f5c88fc928f15896fb16c22b6b8aa34394a
Parneel Bhakhri <hi@pbhak.dev>
2025-12-30 03:21:26 -0800

    use object.hasOwn for input validation

--- src/server/log.ts
+++ src/server/log.ts
@@ -23,7 +23,7 @@ export default async function log(req: Request, res: Response) {
     return;
   }
 
-  if (!req.body.log) {
+  if (!Object.hasOwn(req.body, "log")) {
     sendLog('invalid request made to /log - no log text provided', 'log');
     res.sendStatus(400);
     return;
